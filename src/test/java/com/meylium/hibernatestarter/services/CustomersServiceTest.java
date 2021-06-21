58bea7b241d7f0f430e3451149326dd074d9f2a2
Walid <dw_boubechtoula@esi.dz>
2021-06-21 16:10:10 +0100

    add tests

---
+++ src/test/java/com/meylium/hibernatestarter/services/CustomersServiceTest.java
@@ -0,0 +1,24 @@
+package com.meylium.hibernatestarter.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void testCreate() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void testDelete() {
+    }
+}
---
+++ src/test/java/com/meylium/hibernatestarter/services/OrdersServiceTest.java
@@ -0,0 +1,20 @@
+package com.meylium.hibernatestarter.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void remove() {
+    }
+}
